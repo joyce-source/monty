4a9fa153171ac857cbaa1b775ad50b6b148835a3
joyce-source <brownjojo97@gmail.com>
2023-03-26 12:35:39 -0700

    _update

--- pint.c
+++ pint.c
@@ -10,7 +10,7 @@ void _pint(stack_t **head, unsigned int line_number)
 {
 	if (*head == NULL)
 	{
-		fprintf(stderr, "L%u: can't pint, stack is empty\n", line_number);
+		fprintf(stderr, "L%u: can't pint, stack empty\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 
